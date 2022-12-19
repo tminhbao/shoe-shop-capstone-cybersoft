e212f0408c3d23ad79c2995dc703a42305c58cb4
tminhbao <tminhbao2001@gmail.com>
2022-12-19 08:29:13 +0700

    update header, add google fonts (Inter),  create login page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,23 +3,27 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="bg-dark text-white">
-      <NavLink
-        to="/"
-        className={({ isActive }) =>
-          isActive ? "nav-link active" : "nav-link"
-        }
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/login"
-        className={({ isActive }) =>
-          isActive ? "nav-link active" : "nav-link"
-        }
-      >
-        Login
-      </NavLink>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="bg-dark text-white p-3 nav">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Login
+          </NavLink>
+        </div>
+      </div>
     </div>
   );
 }
