427d995f82c9817e1ecd42dbf75119335c7c576c
tminhbao <tminhbao2001@gmail.com>
2022-12-19 20:12:51 +0700

    create cart page

--- src/components/CartBody/CartBody.jsx
+++ src/components/CartBody/CartBody.jsx
@@ -1,5 +1,59 @@
 import React from "react";
+import pictureProduct from "../../assets/img/image5.png";
+import styles from "./CartBody.module.css";
 
 export default function CartBody() {
-  return <div>CartBody</div>;
+  return (
+    <table className="table">
+      <thead
+        style={{
+          background: "#D9D9D9",
+        }}
+      >
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th>id</th>
+          <th>img</th>
+          <th>name</th>
+          <th>price</th>
+          <th>quantity</th>
+          <th>total</th>
+          <th>action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <input type="checkbox" />
+          </td>
+          <td>1</td>
+          <td>
+            <img src={pictureProduct} alt="" />
+          </td>
+          <td>Product 1</td>
+          <td>1000</td>
+          <td>
+            <button className={styles["btn-quantity"]}>+</button>
+            <span
+              className="quantity"
+              style={{
+                background: "#D9D9D9",
+                padding: "2px 30px",
+              }}
+            >
+              1
+            </span>
+            <button className={styles["btn-quantity"]}>-</button>
+          </td>
+          <td>1000</td>
+          <td>
+            <button className={styles["btn-quantity"]}>Edit</button>
+            <button className={styles["btn-delete"]}>Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import CartBody from "../../components/CartBody/CartBody";
+import styles from "./Cart.module.css";
 
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <p className={styles["cart-title"]}>Carts</p>
+      </div>
+      <div className="row">
+        <CartBody />
+      </div>
+      <div className="row">
+        <button className={styles["btn-submit-order"]}>Submit Order</button>
+      </div>
+    </div>
+  );
 }
