35e3a8875bccb05030faba501d7d6cd69c299452
Truong <truongdai148635@gmail.com>
2022-12-22 21:12:50 +0700

    DAI check

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,6 @@ const Header = () => {
   }
 
   return (
-<<<<<<< HEAD
     <header>
       <nav className="navbar navbar-expand-sm navbar-dark bg-black">
         <div className="container">
@@ -82,30 +81,6 @@ const Header = () => {
         </div>
       </nav>
     </header>
-=======
-    <div className="container-fluid">
-      <div className="row">
-        <div className="bg-dark text-white p-3 nav">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/login"
-            className={({ isActive }) =>
-              isActive ? "nav-link active" : "nav-link"
-            }
-          >
-            Login
-          </NavLink>
-        </div>
-      </div>
-    </div>
->>>>>>> minhbao
   );
 }
 
