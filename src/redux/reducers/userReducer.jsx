12520841563a4c475e5d3a2f7d25b4c04f83a671
tminhbao <tminhbao2001@gmail.com>
2022-12-22 17:09:02 +0700

    handle login and save to localStorage

--- src/redux/reducers/userReducer.jsx
+++ src/redux/reducers/userReducer.jsx
@@ -1,13 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { http, USER_LOGIN, ACCESS_TOKEN } from "../../util/config";
+import { history } from "../../index";
 
-const initialState = {};
+const initialState = {
+  userLogin: null,
+};
 
 const userReducer = createSlice({
   name: "userReducer",
   initialState,
-  reducers: {},
+  reducers: {
+    loginAction: (state, action) => {
+      state.userLogin = action.payload;
+    },
+  },
 });
 
-export const {} = userReducer.actions;
+export const { loginAction } = userReducer.actions;
 
 export default userReducer.reducer;
+
+// Async Action
+
+export const loginApi = (userLogin) => {
+  return async (dispatch) => {
+    const result = await http.post("/api/Users/signin", userLogin);
+    // Cập nhật action cho reducer
+    const action = loginAction(result.data.content);
+    dispatch(action);
+    // Lưu trong localStorage
+    localStorage.setItem(USER_LOGIN, JSON.stringify(result.data.content));
+    localStorage.setItem(
+      ACCESS_TOKEN,
+      JSON.stringify(result.data.content.accessToken)
+    );
+    history.push("/profile");
+  };
+};
--- src/util/config.jsx
+++ src/util/config.jsx
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
+
+export const ACCESS_TOKEN = "access_token";
+export const USER_LOGIN = "user_login";
 
 export const http = axios.create({
-    baseURL: 'https://shop.cyberlearn.vn',
-    timeout: 30000
-})
+  baseURL: "https://shop.cyberlearn.vn",
+  timeout: 30000,
+});
